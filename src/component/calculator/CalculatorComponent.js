e219854137f60de64f7fdf726498fc77a18e1eef
iunter <ivounter@gmail.com>
2022-07-21 20:26:41 -0300

    Calculator with states

--- src/component/calculator/CalculatorComponent.js
+++ src/component/calculator/CalculatorComponent.js
@@ -1,17 +1,33 @@
-import {Box, Flex, SimpleGrid, VStack, Image, Button, IconButton} from "@chakra-ui/react";
-import { BedIcon, BusIcon } from "../icons/icons";
+import { AddIcon, EditIcon, MinusIcon } from "@chakra-ui/icons";
+import {Box, Flex, SimpleGrid, VStack, Image, Button, IconButton, HStack} from "@chakra-ui/react";
+import { BedIcon, BusIcon, DestinationIcon, DollarIcon, HourglassIcon, CalendarIcon } from "../icons/icons";
+import {FaEquals} from "react-icons/fa"
 
-
-export const CalculatorComponent = () =>
-<Flex marginRight={3}>
+export const CalculatorComponent = ({handleClick}) =>
+<Box marginRight={3} bg='gray.300' borderRadius='lg'>
     <VStack>
         <Box bg='green.100' w='calc(20vw)' h='calc(30vh)' margin={5} marginRight={2} borderRadius='lg'>
             
         </Box>
-
-        <SimpleGrid columns={2} spacing={2} w='calc(10vW)'>
-            <IconButton icon={<BedIcon w='calc(5vw)' h='calc(5vh)'/>} />
-            <IconButton icon={<BusIcon w='calc(5vw)' h='calc(5vh)'/>} />
-        </SimpleGrid>
+        <HStack>
+            <SimpleGrid columns={2} spacing={2} w='calc(10vW)' marginBottom={5}>
+                <IconButton icon={<BedIcon w='calc(5vw)' h='calc(5vh)'/>} onClick={() => handleClick("Seleccionar Alojamiento")}/>
+                <IconButton icon={<HourglassIcon w='calc(5vw)' h='calc(5vh)' />}  onClick={() => handleClick("Seleccionar Horarios")} />
+                <IconButton icon={<BusIcon w='calc(5vw)' h='calc(5vh)'/>} onClick={() => handleClick("Seleccionar Transporte")} />
+                <IconButton icon={<DestinationIcon w='calc(5vw)' h='calc(5vh)' />} onClick={() => handleClick("Seleccionar Actividad")}/>
+                <IconButton icon={<CalendarIcon w='calc(5vw)' h='calc(5vh)'/>} onClick={() => handleClick("Seleccionar Dias")}/>
+                <IconButton icon={<DollarIcon w='calc(5vw)' h='calc(5vh)' />} onClick={() => handleClick("Seleccionar Presupuesto")}/>
+            </SimpleGrid>
+            <HStack>
+                <VStack>
+                    <IconButton icon={<MinusIcon />} w='calc(5vw)' h='calc(5vh)'  onClick={() => handleClick("Eliminar")} />
+                    <IconButton icon={<AddIcon />}  w='calc(5vw)' h='calc(10vh)'onClick={() => handleClick("Agregar")}/>
+                </VStack>
+                <VStack>
+                    <IconButton icon={<EditIcon />} w='calc(5vw)' h='calc(5vh)' onClick={() => handleClick("Editar")}/>
+                    <IconButton icon={<FaEquals />}  w='calc(5vw)' h='calc(10vh)' onClick={() => handleClick("Calcular")}/>
+                </VStack>
+            </HStack>
+        </HStack>
     </VStack>
-</Flex>
+</Box>
--- src/component/pages/CalculatorPage.js
+++ src/component/pages/CalculatorPage.js
@@ -1,15 +1,27 @@
 import {Box, Flex, Grid, GridItem, SimpleGrid, VStack, Image, Button, IconButton} from "@chakra-ui/react";
+import { useState } from "react";
 import { CalculatorComponent } from "../calculator/CalculatorComponent";
 import { BedIcon, BusIcon } from "../icons/icons";
 
-export const CalculatorPage = () =>
+export const CalculatorPage = () =>{
+
+    const [message, setMessage] = useState("");
+
+    const handleClick = (newMessage) =>
+    {
+        setMessage(newMessage);
+    }
+
+    return(
     <Grid templateColumns='repeat(2,1fr)' gap={6} bg='orange.100'>
         <GridItem maxWidth='calc(50vw)' maxHeight='calc(100vh)' bg='white'>
-            <CalculatorComponent/>
+            <CalculatorComponent handleClick={handleClick}/>
         </GridItem>
         <GridItem maxWidth='calc(50vw)' maxHeight='calc(100vh)' bg='white'>
             <Flex marginLeft={3}>
-            Aca va lo calculado
+                {message}
             </Flex>
         </GridItem>
     </Grid>
+);
+}
