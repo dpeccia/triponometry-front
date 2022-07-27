39a8c7ebfc0cc878d6227e2ac98234b39db6ebfd
lmendoza01 <lucasfmendoza@gmail.com>
2022-07-26 22:54:40 -0300

    Adding ResultMap component

---
+++ src/component/map/ResultMap.js
@@ -0,0 +1,2 @@
+export const ResultMap = () =>
+<div>MAPA</div>
--- src/component/pages/ResultPage.js
+++ src/component/pages/ResultPage.js
@@ -1,6 +1,32 @@
-import {Flex} from "@chakra-ui/react";
+import {Box, Flex} from "@chakra-ui/react";
+import { ResultMap } from "../map/ResultMap";
 
-export const ResultPage = () =>
-    <Flex>
-        Aqui va el dasd
+export const ResultPage = () => {
+
+    // const handleClick = (inputComponent) =>
+    // {
+    //     setInputComponent(inputComponent);
+    // }
+    
+    // const [city, setCity] = useState();
+    // const [accommodation,setAccommodation] = useState();
+    // const [inputComponent, setInputComponent] = useState(<CityInput setCity={setCity} setAccommodation={setAccommodation} nextStep={handleClick}/>);
+
+    // console.log(accommodation)
+    return(
+    <Flex flexDirection='row'>
+        <Box>
+            InfoCalculation
+        </Box>
+        
+        <Flex marginLeft={3} alignItems="flex-start">
+            <ResultMap></ResultMap>
+        </Flex>
+
+        <Flex marginLeft={3} alignItems="flex-start">
+            Calendar
+        </Flex>
     </Flex>
+    )
+}
+    
