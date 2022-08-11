c3a87616ba6fcd38a559a369787e1c94d3325e2f
lfm41301 <lucas.f.mendoza@gsk.com>
2022-08-10 21:20:31 -0300

    calling Kml onLoad

--- src/component/map/ResultMap.js
+++ src/component/map/ResultMap.js
@@ -1,4 +1,4 @@
-import { GoogleMap, KmlLayer, LoadScript} from "@react-google-maps/api";
+import { GoogleMap, KmlLayer, LoadScript, } from "@react-google-maps/api";
 import { Component } from "react"
 
 const containerStyle = {
@@ -8,23 +8,29 @@ const containerStyle = {
 
 export class ResultMap extends Component {
 
-   //Habria que cambiar test por el id del viaje
-  kmlTest ="https://yji8each99.execute-api.us-east-1.amazonaws.com/dev/agenda/kml/test";
+  //Habria que cambiar test por el id del viaje
+ kmlTest ="https://yji8each99.execute-api.us-east-1.amazonaws.com/dev/agenda/kml/test";
 
-  render() {
+ render() {
 
-    return (
-      <div className="App">
-      <LoadScript googleMapsApiKey="AIzaSyAIQZSE4hWZYz9YcyNuTCSjjs6j3jObME0">
-        <GoogleMap
-          center={{ lat: 48.865127, lng: 2.350331 }}
-          mapContainerStyle={containerStyle}
-          zoom={4}
-        >
-          <KmlLayer url={this.kmlTest}/>
-        </GoogleMap>
-      </LoadScript>
-    </div>
-    )
-  }
+   return (
+     <div className="App">
+     <LoadScript googleMapsApiKey="AIzaSyAIQZSE4hWZYz9YcyNuTCSjjs6j3jObME0">
+       <GoogleMap
+         center={{ lat: 48.865127, lng: 2.350331 }}
+         mapContainerStyle={containerStyle}
+         zoom={4}
+         onLoad={map => {
+            new window.google.maps.KmlLayer(this.kmlTest, {
+            suppressInfoWindows: true,
+            preserveViewport: false,
+            map: map
+          });
+        }}
+       >
+       </GoogleMap>
+     </LoadScript>
+   </div>
+   )
+ }
 }
