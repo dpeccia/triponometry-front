e2a1d308c4d6b0391d8e674a13abab831852ca71
lfm41301 <lucas.f.mendoza@gsk.com>
2022-08-20 09:43:45 -0300

    Refactoring ICalendar

--- src/component/calendar/Icalendar.js
+++ src/component/calendar/Icalendar.js
@@ -1,25 +1,40 @@
 import React from "react";
-import ICalendarLink from "react-icalendar-link";
 
 export class ICalendar extends React.Component {
 
+  rawContent = "";
+
+  constructor(props) {
+    super(props);
+    this.loadRawContent();
+  }
+
+  //El raw va a venir apuntando a endpoint aparte que no está creado todavía, despues hay que tocar este metodo
+  loadRawContent(){
+      fetch("")
+      .then(r => r.text())
+      .then(text => {
+        this.rawContent = text;
+      });
+  }
 
   render() {
-    const event = {
-      title: "My Title",
-      description: "My Description",
-      startTime: "2018-10-07T10:30:00+10:00",
-      location: "10 Carlotta St, Artarmon NSW 2064, Australia"
-    }
-    const rawContent = `ATTENDEE;CN="Cyrus Daboo";CUTYPE=INDIVIDUAL;PARTSTAT=ACCEPTED:mailto:cyrus@example.com
-ATTENDEE;CN="Wilfredo Sanchez Vega";CUTYPE=INDIVIDUAL;PARTSTAT=NEEDS-ACTION;ROLE=REQ-PARTICIPANT;RSVP=TRUE:mailto:wilfredo@example.com
-ATTENDEE;CN="Bernard Desruisseaux";CUTYPE=INDIVIDUAL;PARTSTAT=NEEDS-ACTION;ROLE=REQ-PARTICIPANT;RSVP=TRUE:mailto:bernard@example.net
-ATTENDEE;CN="Mike Douglass";CUTYPE=INDIVIDUAL;PARTSTAT=NEEDS-ACTION;RSVP=TRUE:mailto:mike@example.org`;
+
+    const downloadTxtFile = () => {
+      const element = document.createElement("a");
+      const file = new Blob([this.rawContent], {
+        type: "text/plain"
+      });
+      element.href = URL.createObjectURL(file);
+      element.download = "myFile.ics";
+      document.body.appendChild(element);
+      element.click();
+    };
 
     return (
-      <ICalendarLink event={event} rawContent={rawContent}>
-        Download
-      </ICalendarLink>
+      <div>
+        <button onClick={downloadTxtFile}>Download Calendar</button>
+      </div>
     );
   }
 }
