77f7d33488d2529f50ef7fefa99d6b90da3abdd9
lfm41301 <lucas.f.mendoza@gsk.com>
2022-08-27 17:19:14 -0300

    Agregando MapExport al ResultMap

--- src/component/map/ResultMap.js
+++ src/component/map/ResultMap.js
@@ -1,12 +1,15 @@
 import { GoogleMap } from "@react-google-maps/api";
+import { MapExport } from "./MapExport";
 
 const containerStyle = {
   width: '100%', height: '350px'
 };
 
 export const ResultMap = ({ mapId, accommodation }) => {
-  const kmlTest =`https://yji8each99.execute-api.us-east-1.amazonaws.com/dev/s3/${mapId}.xml`;
+
+  const kmlTest =`https://yji8each99.execute-api.us-east-1.amazonaws.com/dev/s3/${mapId}`;
   return (
+    <div>
     <GoogleMap
       center={{ lat: accommodation.latitude, lng: accommodation.longitude }}
       mapContainerStyle={containerStyle}
@@ -18,5 +21,7 @@ export const ResultMap = ({ mapId, accommodation }) => {
           map: map
         });
     }}/>
+    <MapExport kmlId={mapId}></MapExport>
+    </div>
   );
 }
